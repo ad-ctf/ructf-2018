078b3a61d79d490618c9bc44552c935c4cd66197
hacker-volodya <3119142+hacker-volodya@users.noreply.github.com>
2026-03-23 11:50:33 +0300

    fix(hologram): keep chemical-element header generation

--- checkers/Hologram/actions/utils.py
+++ checkers/Hologram/actions/utils.py
@@ -1,6 +1,30 @@
 import random
 
-from mimesis import Food, Text
+from mimesis import Choice, Food
+
+CHEMICAL_ELEMENTS = [
+    "Hydrogen", "Helium", "Lithium", "Beryllium", "Boron", "Carbon",
+    "Nitrogen", "Oxygen", "Fluorine", "Neon", "Sodium", "Magnesium",
+    "Aluminium", "Silicon", "Phosphorus", "Sulfur", "Chlorine", "Argon",
+    "Potassium", "Calcium", "Scandium", "Titanium", "Vanadium", "Chromium",
+    "Manganese", "Iron", "Cobalt", "Nickel", "Copper", "Zinc", "Gallium",
+    "Germanium", "Arsenic", "Selenium", "Bromine", "Krypton", "Rubidium",
+    "Strontium", "Yttrium", "Zirconium", "Niobium", "Molybdenum",
+    "Technetium", "Ruthenium", "Rhodium", "Palladium", "Silver", "Cadmium",
+    "Indium", "Tin", "Antimony", "Tellurium", "Iodine", "Xenon", "Cesium",
+    "Barium", "Lanthanum", "Cerium", "Praseodymium", "Neodymium",
+    "Promethium", "Samarium", "Europium", "Gadolinium", "Terbium",
+    "Dysprosium", "Holmium", "Erbium", "Thulium", "Ytterbium", "Lutetium",
+    "Hafnium", "Tantalum", "Tungsten", "Rhenium", "Osmium", "Iridium",
+    "Platinum", "Gold", "Mercury", "Thallium", "Lead", "Bismuth",
+    "Polonium", "Astatine", "Radon", "Francium", "Radium", "Actinium",
+    "Thorium", "Protactinium", "Uranium", "Neptunium", "Plutonium",
+    "Americium", "Curium", "Berkelium", "Californium", "Einsteinium",
+    "Fermium", "Mendelevium", "Nobelium", "Lawrencium", "Rutherfordium",
+    "Dubnium", "Seaborgium", "Bohrium", "Hassium", "Meitnerium",
+    "Darmstadtium", "Roentgenium", "Copernicium", "Nihonium", "Flerovium",
+    "Moscovium", "Livermorium", "Tennessine", "Oganesson",
+]
 
 top = 200000000
 down = -200000000
@@ -11,9 +35,9 @@ def get_random_cords():
 
 
 def get_random_header():
-    generator = Text("en")
-    first = generator.word().title()
-    second = generator.word().title()
+    generator = Choice()
+    first = generator.choice(CHEMICAL_ELEMENTS)
+    second = generator.choice(CHEMICAL_ELEMENTS)
     return "{} {}ous #{}".format(first, second, random.randint(1, 1000))
 
 
